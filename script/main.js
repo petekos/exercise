a1f917be5b724ff583e0c2a69f249e7d8cc7f19c
pkosa <petekos@gmail.com>
2014-12-09 01:13:45 -0800

    lots of changes
    added resizing; changed styles; added image preloading; added keyboard
    navigation

--- script/main.js
+++ script/main.js
@@ -11,21 +11,26 @@ AJAX.request = function(method, url, callback) {
 var PhotoViewer = (function() {
 	var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=93971dd7e74a67b5a30fa23b9def8f8e&photoset_id=72157626579923453&format=json&nojsoncallback=1",
 	    photos = [],
-	    currentPhoto = 0,
+	    currentPhotoIdx = 0,
+	    widthToHeight = 0,
+	    heightToWidth = 0,
 	    isLoading = false,
-	    loadScreenTimeoutID;
+	    loadScreenTimeoutID,
+	    currentPreloadIdx = 1,// no need to preload the 0th image
+	    preloadAmount = 5;
 
 	function loadPhotos() {
 		var response = JSON.parse(this.responseText);
-		console.log(response)
-		if (response.hasOwnProperty('photoset')){
+		if (response.hasOwnProperty('photoset')) {
 			photos = response.photoset.photo;	
 		} else {
 			console.error("Response has no photoset property");	
 		}
-		if(photos.length > 0){
-			showPhoto(photos[currentPhoto]);	
-		}else {
+
+		if (photos.length > 0) {
+			showPhoto(photos[currentPhotoIdx]);
+			preloadImages();	
+		} else {
 			console.error("Photoset has no photos");	
 		}
 	}
@@ -34,42 +39,56 @@ var PhotoViewer = (function() {
     	return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_z.jpg";
     }
 
+    function preloadImages() {
+    	for( var i = currentPreloadIdx; i < (currentPhotoIdx + preloadAmount); i++) {
+    		var img = new Image();
+    		img.src =buildPhotoUrl(photos[i]);
+    		currentPreloadIdx++;
+    	}
+    }
+
 	function showPhoto(photo) {
-		loading(true);
 		var img = document.querySelector(".display-image");
+		loading(true);
 		img.src = buildPhotoUrl(photo);
-		
+		preloadImages();
 		img.onload= function () {
-			loading(false)
+			loading(false);
+			widthToHeight = img.naturalWidth/img.naturalHeight;
+			heightToWidth = img.naturalHeight/img.naturalWidth;
+			document.querySelector(".overlay-content").style.maxWidth = img.naturalWidth +"px";	
+			document.querySelector(".overlay-content").style.maxHeight = img.naturalHeight + "px";	
 			document.querySelector(".title").textContent = photo.title;
 			document.querySelector(".overlay-container").style.display = "block";
-
+			resize();
 		}
 	}
 
     function showNext(){
-    	if(currentPhoto == photos.length - 1){
-    		currentPhoto = 0; 
-    	} else{
-    		currentPhoto = currentPhoto + 1;	
+    	if (currentPhotoIdx == photos.length - 1) {
+    		currentPhotoIdx = 0; 
+    	} else {
+    		currentPhotoIdx = currentPhotoIdx + 1;	
     	}
 		
-		showPhoto(photos[currentPhoto]);
+		showPhoto(photos[currentPhotoIdx]);
     }
 
     function showPrevious(){
-    	if(currentPhoto == 0 ) {
-    		currentPhoto = photos.length - 1;
+    	if(currentPhotoIdx == 0 ) {
+    		currentPhotoIdx = photos.length - 1;
     	} else {
-    		currentPhoto = currentPhoto - 1;	
+    		currentPhotoIdx = currentPhotoIdx - 1;	
     	}
 		
-    	showPhoto(photos[currentPhoto]);
+    	showPhoto(photos[currentPhotoIdx]);
     }
 
     function bindEvents(){
     	document.querySelector(".previous").addEventListener("click",showPrevious.bind(this));
     	document.querySelector(".next").addEventListener("click",showNext.bind(this));
+    	window.addEventListener("resize", PhotoViewer.resize);
+		window.addEventListener("keyup", PhotoViewer.keyNav);
     }
 
     function loading(loading){
@@ -87,28 +106,54 @@ var PhotoViewer = (function() {
 	    			
 	    	}else {
 	    		window.clearTimeout(loadScreenTimeoutID);
-	    		console.log(screenEl.firstChild);
 	    		if(screenEl.firstChild) {
 	    			screenEl.style.zIndex = 0;
-
 	    			screenEl.removeChild(screenEl.firstChild);
 	    		}
-				
-
 	    	}
+    	}
+    }
+
+    function resize(){
+    	var width = document.documentElement.clientWidth,
+    	    height = document.documentElement.clientHeight,
+    	    overlay = document.querySelector(".overlay-content"),
+    	    proposedHeight = 0,
+    	    proposedWidth = 0;
+
+    	proposedHeight  = (height - 80);
+    	proposedWidth = (width - 80);
+    	if (proposedHeight < proposedWidth * heightToWidth ) {
+    		//start from height
+    		overlay.style.width = (proposedHeight * widthToHeight)+"px";
+    		overlay.style.height = proposedHeight +"px";
+    	} else {
+    		//start from width
+    		overlay.style.width = proposedWidth +"px"; 
+    		overlay.style.height = (proposedWidth * heightToWidth) +"px";
+    	}
+    }
+
+    function keyNav(e){
+    	if (e.keyCode ==37) {
+    		showPrevious();
+    	} else if (e.keyCode == 39) {
+    		showNext();
     	}
     }
 
 	return {
 		url: url,
-		loadPhotos : loadPhotos,
-		bindEvents : bindEvents,
-		loading : loading
+		loadPhotos: loadPhotos,
+		bindEvents: bindEvents,
+		loading : loading,
+		resize: resize,
+		keyNav: keyNav
 	};
 })();
 
 window.onload = function(){
-	PhotoViewer.loading(true)
+	PhotoViewer.loading(true);
 	PhotoViewer.bindEvents();
 	AJAX.request("get", PhotoViewer.url, PhotoViewer.loadPhotos);
 };
