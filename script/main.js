a0fa042678bc34bdfd97e0d307b5edc8022777cb
pkosa <petekos@gmail.com>
2014-12-10 01:44:02 -0800

    lots of changes
    restyled loading screen; resize arrows when image is very small; added
    prefetching;

--- script/main.js
+++ script/main.js
@@ -1,6 +1,35 @@
 'use strict'
-var AJAX = {};
+// a couple util methods ...these shouldnt be declared in the global scope
+var query = document.querySelector.bind(document);
 
+function throttle(callback, limit){
+	var wait = false;
+	return function() {
+		if (!wait) {
+			callback.call();
+			wait = true;
+			setTimeout(function() {
+				wait = false;
+			}, limit);
+		}
+	};
+} 
+
+function removeClass(className, classString) {
+	var classes = classString.split(" ");
+	if(classes.length > 0 ){
+		for (var i = 0; i < classes.length; i++) {
+			if(className == classes[i]){
+				classes[i] = "";
+				break;
+			}
+		}
+		return classes.join(" ");	
+	}
+	
+}
+
+var AJAX = {};
 AJAX.request = function(method, url, callback) {
 	var req = new XMLHttpRequest();
 	req.onload = callback;
@@ -8,142 +37,209 @@ AJAX.request = function(method, url, callback) {
 	req.send();
 };
 
-var PhotoViewer = (function() {
-	var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=93971dd7e74a67b5a30fa23b9def8f8e&photoset_id=72157626579923453&format=json&nojsoncallback=1",
-	    photos = [],
-	    currentPhotoIdx = 0,
-	    widthToHeight = 0,
-	    heightToWidth = 0,
-	    isLoading = false,
-	    loadScreenTimeoutID,
-	    currentPreloadIdx = 1,// no need to preload the 0th image
-	    preloadAmount = 5;
+var PhotoViewer = (function() {  
+	var photos = [],
+		currentPhotoIdx = 0,
+		widthToHeight = 0,
+		heightToWidth = 0,
+		isLoading = false,
+		loadScreenTimeoutID,
+		currentPreloadIdx = 1,// no need to preload the 0th image
+		preload = 5,
+		prefetch = 3,
+		pages = 0,
+		currentPage = 1;
+
+	function buildApiUrl(page) {
+		return "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=93971dd7e74a67b5a30fa23b9def8f8e&photoset_id=72157626579923453&per_page=10&page="+page+"&format=json&nojsoncallback=1"
+	}
 
 	function loadPhotos() {
 		var response = JSON.parse(this.responseText);
-		if (response.hasOwnProperty('photoset')) {
-			photos = response.photoset.photo;	
+		if (!response.hasOwnProperty('photoset')) {	
+			console.error("Response has no photoset property");
 		} else {
-			console.error("Response has no photoset property");	
-		}
-
-		if (photos.length > 0) {
-			showPhoto(photos[currentPhotoIdx]);
-			preloadImages();	
-		} else {
-			console.error("Photoset has no photos");	
+			pages = response.photoset.pages;
+			if (response.photoset.photo.length > 0) {
+				photos = photos.concat(response.photoset.photo);
+				showPhoto(photos[currentPhotoIdx]);
+			} else {
+				console.error("Photoset has no photos");	
+			}
 		}
 	}
 
-    function buildPhotoUrl(photo) {
-    	return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_z.jpg";
-    }
+	function enableNavigation() {
 
-    function preloadImages() {
-    	for( var i = currentPreloadIdx; i < (currentPhotoIdx + preloadAmount); i++) {
-    		var img = new Image();
-    		img.src =buildPhotoUrl(photos[i]);
-    		currentPreloadIdx++;
-    	}
-    }
+		if (currentPhotoIdx == 0) {
+			query(".previous").style.visibility = "hidden"; 
+		} else {
+			query(".previous").style.visibility = "visible"; 
+		}
+
+		if (currentPhotoIdx == (photos.length - 1)) {
+			query(".next").style.visibility = "hidden";
+		} else {
+			query(".next").style.visibility = "visible"; 
+		}
+	}
+
+	function buildPhotoUrl(photo) {
+		return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_z.jpg";
+	}
+
+	function preloadImages() {
+		for (var i = currentPreloadIdx; i < (currentPhotoIdx + preload); i++) {
+			if(i < photos.length - 1){
+				setTimeout(function(){
+					var img = new Image();
+			    	img.src = buildPhotoUrl(photos[i]);
+				},30)
+			    currentPreloadIdx++;
+			}
+			
+		}
+	}
+
+	function prefetchResults() {
+		if (currentPage < pages && (currentPhotoIdx >= (photos.length - prefetch ))) {
+			currentPage++;
+			AJAX.request("get", PhotoViewer.buildApiUrl(currentPage), PhotoViewer.loadPhotos);	
+		}	
+	}
 
 	function showPhoto(photo) {
-		var img = document.querySelector(".display-image");
+		var img = query(".display-image");
+
 		loading(true);
 		img.src = buildPhotoUrl(photo);
+		prefetchResults();
 		preloadImages();
-		img.onload= function () {
-			loading(false);
-			widthToHeight = img.naturalWidth/img.naturalHeight;
-			heightToWidth = img.naturalHeight/img.naturalWidth;
-			document.querySelector(".overlay-content").style.maxWidth = img.naturalWidth +"px";	
-			document.querySelector(".overlay-content").style.maxHeight = img.naturalHeight + "px";	
-			document.querySelector(".title").textContent = photo.title;
-			document.querySelector(".overlay-container").style.display = "block";
-			resize();
+		if(!img.complete){
+			img.onload = function (event) {
+					onImageLoad(img);
+			};
+		} else {
+			onImageLoad(img);
 		}
 	}
 
-    function showNext(){
-    	if (currentPhotoIdx == photos.length - 1) {
-    		currentPhotoIdx = 0; 
-    	} else {
-    		currentPhotoIdx = currentPhotoIdx + 1;	
-    	}
-		
+	function onImageLoad(img) {
+		loading(false);
+		enableNavigation();
+		widthToHeight = img.naturalWidth/img.naturalHeight;
+		heightToWidth = img.naturalHeight/img.naturalWidth;
+		query(".overlay-content").style.maxWidth = img.naturalWidth +"px";	
+		query(".overlay-content").style.maxHeight = img.naturalHeight + "px";	
+		query(".title-text").textContent = photos[currentPhotoIdx].title;
+		query(".overlay-container").style.display = "block";
+		resize();
+	}
+
+	function openCurrentPhoto() {
+		window.open(buildPhotoUrl(photos[currentPhotoIdx]),"_blank");
+	}
+
+	function showNext() {
+		if (currentPhotoIdx == photos.length - 1) {
+			return;
+		} else {
+			currentPhotoIdx++;	
+		}
 		showPhoto(photos[currentPhotoIdx]);
-    }
+	}
 
-    function showPrevious(){
-    	if(currentPhotoIdx == 0 ) {
-    		currentPhotoIdx = photos.length - 1;
-    	} else {
-    		currentPhotoIdx = currentPhotoIdx - 1;	
-    	}
-		
-    	showPhoto(photos[currentPhotoIdx]);
-    }
+	function showPrevious() {
+		if (currentPhotoIdx == 0 ) {
+			return;
+		} else {
+			currentPhotoIdx--;	
+		}
+		showPhoto(photos[currentPhotoIdx]);
+	}
 
-    function bindEvents(){
-    	document.querySelector(".previous").addEventListener("click",showPrevious.bind(this));
-    	document.querySelector(".next").addEventListener("click",showNext.bind(this));
-    	window.addEventListener("resize", PhotoViewer.resize);
+	function closePhoto() {
+		query(".overlay-container").style.display="none";
+		query(".screen").style.display="none";
+	}
+
+	function bindEvents() {
+		query(".previous").addEventListener("click",showPrevious.bind(this));
+		query(".next").addEventListener("click",showNext.bind(this));
+		window.addEventListener("resize", throttle(PhotoViewer.resize,100));
 		window.addEventListener("keyup", PhotoViewer.keyNav);
-    }
+		query(".close").addEventListener("click", closePhoto);
+		query(".external-link").addEventListener("click", openCurrentPhoto );
+	}
 
-    function loading(loading){
-    	if(isLoading != loading ){
+	function loading(loading) {
+		if(isLoading != loading) {
 			isLoading = loading;
-	    	var screenEl = document.querySelector(".screen");
-	    	if(loading){
-	    		loadScreenTimeoutID = setTimeout(function(){
-	    			var loadingEl = document.createElement("div");
-	    			var loadingText = document.createTextNode("LOADING"); 
-	  				loadingEl.appendChild(loadingText); 
-	    			screenEl.appendChild(loadingEl);
-	    			screenEl.style.zIndex = 2;
-	    		},300);
-	    			
-	    	}else {
-	    		window.clearTimeout(loadScreenTimeoutID);
-	    		if(screenEl.firstChild) {
-	    			screenEl.style.zIndex = 0;
-	    			screenEl.removeChild(screenEl.firstChild);
-	    		}
-	    	}
-    	}
-    }
+			var screenEl = query(".screen");
+			screenEl.style.display = "block";
+			if (loading) {
+				loadScreenTimeoutID = setTimeout(function() {
+					query(".loading").style.display = "block";
+					screenEl.style.zIndex = 2;
+				},300);
+					
+			} else {
+				window.clearTimeout(loadScreenTimeoutID);
+				
+				screenEl.style.zIndex = 0;
+				query(".loading").style.display = "none";
+				
+			}
+		}
+	}
 
-    function resize(){
-    	var width = document.documentElement.clientWidth,
-    	    height = document.documentElement.clientHeight,
-    	    overlay = document.querySelector(".overlay-content"),
-    	    proposedHeight = 0,
-    	    proposedWidth = 0;
+	function resize() {
+		var width = document.documentElement.clientWidth,
+			height = document.documentElement.clientHeight,
+			overlay = query(".overlay-content"),
+			proposedHeight = 0,
+			proposedWidth = 0,
+			finalHeight = 0,
+			finalWidth = 0;
 
-    	proposedHeight  = (height - 80);
-    	proposedWidth = (width - 80);
-    	if (proposedHeight < proposedWidth * heightToWidth ) {
-    		//start from height
-    		overlay.style.width = (proposedHeight * widthToHeight)+"px";
-    		overlay.style.height = proposedHeight +"px";
-    	} else {
-    		//start from width
-    		overlay.style.width = proposedWidth +"px"; 
-    		overlay.style.height = (proposedWidth * heightToWidth) +"px";
-    	}
-    }
+		proposedHeight  = (height - 80);
+		proposedWidth = (width - 120);
+		if (proposedHeight < proposedWidth * heightToWidth ) {
+			//start from height
+			finalWidth = (proposedHeight * widthToHeight);
+			finalHeight = proposedHeight;
+		} else {
+			//start from width
+			finalWidth = proposedWidth; 
+			finalHeight = (proposedWidth * heightToWidth);
+		}
 
-    function keyNav(e){
-    	if (e.keyCode ==37) {
-    		showPrevious();
-    	} else if (e.keyCode == 39) {
-    		showNext();
-    	}
-    }
+		if (finalHeight < 200) {
+			query(".overlay-container").className += " small";
+		} else {
+			query(".overlay-container").className = removeClass("small", query(".overlay-container").className);
+		}
+		overlay.style.width = finalWidth + "px";
+		overlay.style.height = finalHeight + "px";
+	}
+
+	function keyNav(e) {
+		switch (e.keyCode) {
+			case 27:
+				closePhoto();
+				break;
+			case 37:
+				showPrevious();
+				break;
+			case 39:
+				showNext();	
+				break;
+			default:
+		}
+	}
 
 	return {
-		url: url,
+		buildApiUrl: buildApiUrl,
 		loadPhotos: loadPhotos,
 		bindEvents: bindEvents,
 		loading : loading,
@@ -152,8 +248,10 @@ var PhotoViewer = (function() {
 	};
 })();
 
-window.onload = function(){
-	PhotoViewer.loading(true);
-	PhotoViewer.bindEvents();
-	AJAX.request("get", PhotoViewer.url, PhotoViewer.loadPhotos);
+window.onload = function() {
+	query("button").addEventListener("click",function() {
+		PhotoViewer.loading(true);
+		PhotoViewer.bindEvents();
+		AJAX.request("get", PhotoViewer.buildApiUrl(1), PhotoViewer.loadPhotos);
+	});	
 };
